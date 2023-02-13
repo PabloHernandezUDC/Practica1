9eab2fbb74eb01ea6bad41b60aee940c9fe520c4
PabloHernandezUDC <125274113+PabloHernandezUDC@users.noreply.github.com>
2023-02-13 14:29:31 +0100

    sobran cosas

--- item.py
+++ item.py
@@ -2,3 +2,51 @@ class item():
     class weapon():
         def __init__(self):
             self.power = int
+            
+        def get_power(self):
+            return self.power
+        
+        def set_power(self, inputPower):
+            self.get_power = inputPower
+        
+        class sword():
+            def __init__(self):
+                self.name = str
+            def get_name(self):
+                return self.name
+            def set_name(self, inputName):
+                self.name = inputName
+
+        class wand():
+            def __init__(self):
+                self.name = str
+            def get_name(self):
+                return self.name
+            def set_name(self, inputName):
+                self.name = inputName
+
+    class covering():
+        def __init__(self):
+            self.protection = int
+            
+        def get_protection(self):
+            return self.protection
+        
+        def set_protection(self, inputProtection):
+            self.protection = inputProtection
+        
+        class armor():
+            def __init__(self):
+                self.name = str
+            def get_name(self):
+                return self.name
+            def set_name(self, inputName):
+                self.name = inputName
+
+        class shield():
+            def __init__(self):
+                self.name = str
+            def get_name(self):
+                return self.name
+            def set_name(self, inputName):
+                self.name = inputName    
