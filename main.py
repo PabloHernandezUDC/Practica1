5db100b8f73efab9f1b6283725eb9b9097f0099b
PabloHernandezUDC <125274113+PabloHernandezUDC@users.noreply.github.com>
2023-03-07 11:22:57 +0100

    ahora lo devuelve en un formato precioso

--- main.py
+++ main.py
@@ -105,7 +105,6 @@ if __name__ == "__main__":
     n_of_simulations = 30
     list_of_characters = run(sys.argv[1])[0]
     winners, finalPlayerStats, finalHealStats = {}, {}, {}
-    usingPriest = True
     winsPerClass = {
         'Warrior': 0,
         'Mage': 0,
@@ -116,9 +115,15 @@ if __name__ == "__main__":
         'Warrior': [],
         'Mage': [],
     }
-    if usingPriest:
-        finalClassStats['Priest'] = []
-        
+    
+    for i in list_of_characters:
+        if "Priest" in i.get_name():
+            usingPriest = True
+            finalClassStats['Priest'] = []
+            break
+        else:
+            usingPriest = False
+
     finalClassHealStats = {
         'Warrior': [],
         'Mage': [],
@@ -182,34 +187,42 @@ if __name__ == "__main__":
             desviacion = statistics.stdev(finalClassHealStats[i], healMedio)
             finalClassHealStats[i] = [healMedio, desviacion]
 
-    # ordenamos los diccionarios en orden descendente y los imprimimos
+    print('\nNº de victorias por personaje:')
+    print('--------------------------------------------')
     sortedWinners = sorted(winners.items(), key=lambda x:x[1], reverse=True)
     for i in sortedWinners:
         if i[1] != 0:
-            print('El personaje {one} ha ganado {two} veces.'.format(one = i[0], two = i[1]))
-    print('El resto no han ganado nunca.\n')
+            print('Nombre: {one}   \t| nº de victorias: {two}'.format(one = i[0], two = i[1]))
+    print('El resto no han ganado nunca.')
     
+    print('\nEstadísticas de daño por personaje:')
+    print('--------------------------------------------------------------------------')
     for i in finalPlayerStats:
-            print('El personaje {one} ha tenido una media de daño de {two} y una desviación típica de {three}.'.format(one = i, two = finalPlayerStats[i][0], three = finalPlayerStats[i][1]))
-    print()
+            print('Nombre: {one}   \t| Daño medio: {two}   \t| Desviación típica: {three}'.format(one = i, two = round(finalPlayerStats[i][0], 2), three = round(finalPlayerStats[i][1], 2)))
     
+    print('\nNº de victorias por clase:')
+    print('--------------------------------------')
     sortedWinsPerClass = sorted(winsPerClass.items(), key=lambda x:x[1], reverse=True)
     for i in sortedWinsPerClass:
         if i[1] != 0:
-            print('La clase {one} ha ganado {two} veces.'.format(one = i[0], two = i[1]))
-    print()
+            print('Clase: {one}\t| nº de victorias: {two}'.format(one = i[0], two = i[1]))
 
+    print('\nEstadísticas de daño por clase:')
+    print('-----------------------------------------------------------------')
     for i in finalClassStats:
-            print('La clase {one} ha tenido una media de daño de {two} y una desviación típica de {three}.'.format(one = i, two = finalClassStats[i][0], three = finalClassStats[i][1]))
-    print()
+            print('Clase: {one}\t| Daño medio: {two}\t| Desviación típica: {three}'.format(one = i, two = round(finalClassStats[i][0], 2), three = round(finalClassStats[i][1], 2)))
     
-    # healing stats
-    for i in finalHealStats:
-        if len(finalHealStats[i]) > 0:
-            print('El personaje {one} ha tenido una media de curación de {two} y una desviación típica de {three}.'.format(one = i, two = finalHealStats[i][0], three = finalHealStats[i][1]))
-    print()
     
-    for i in finalClassHealStats:
-        if len(finalClassHealStats[i]) > 0:
-            print('La clase {one} ha tenido una media de curación de {two} y una desviación típica de {three}.'.format(one = i, two = finalClassHealStats[i][0], three = finalClassHealStats[i][1]))
+    if usingPriest:
+        print('\nEstadísticas de curación por personaje:')
+        print('-------------------------------------------------------------------------')
+        for i in finalHealStats:
+            if len(finalHealStats[i]) > 0:
+                print('Nombre: {one}   \t| Curación media: {two}\t| Desviación típica: {three}'.format(one = i, two = round(finalHealStats[i][0], 2), three = round(finalHealStats[i][1], 2)))
+
+        print('\nEstadísticas de curación por clase:')
+        print('-----------------------------------------------------------------')
+        for i in finalClassHealStats:
+            if len(finalClassHealStats[i]) > 0:
+                print('Clase: {one}\t| Curación media: {two}\t| Desviación típica: {three}'.format(one = i, two = round(finalClassHealStats[i][0], 2), three = round(finalClassHealStats[i][1], 2)))
     print()
